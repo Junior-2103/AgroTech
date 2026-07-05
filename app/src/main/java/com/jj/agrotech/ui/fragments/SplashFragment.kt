14ccb9b5554371bd883a3d7f44b5db145beda9a5
Junior-2103 <jjosewilker45@proton.me>
2026-07-05 19:50:00 -0300

    feat: Adicionar a tela básica

---
+++ app/src/main/java/com/jj/agrotech/ui/fragments/SplashFragment.kt
@@ -0,0 +1,31 @@
+package com.jj.agrotech.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.jj.agrotech.R
+import com.jj.agrotech.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+
+    lateinit var binding: FragmentSplashBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSplashBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
