c24421b617c238b357ac35f3955da6937b4ec504
Junior-2103 <jjosewilker45@proton.me>
2026-07-05 22:04:13 -0300

    feat: Chamar a função placePlantas()

--- app/src/main/java/com/jj/agrotech/ui/fragments/SplashFragment.kt
+++ app/src/main/java/com/jj/agrotech/ui/fragments/SplashFragment.kt
@@ -5,8 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.jj.agrotech.R
+import com.jj.agrotech.data.repository.PlantaRepository
 import com.jj.agrotech.databinding.FragmentSplashBinding
+import kotlinx.coroutines.launch
 
 class SplashFragment : Fragment() {
 
@@ -26,6 +30,13 @@ class SplashFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            val repository = PlantaRepository(requireContext())
+            repository.placePlantas()
+
+            findNavController().navigate(R.id.action_splashFragment_to_welcomeFragment)
+        }
 
     }
 }
