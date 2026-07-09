1f1ed71935a4d309873c8c82df866914c8542d62
Junior-2103 <jjosewilker45@proton.me>
2026-07-09 18:00:26 -0300

    style: Mudar cores do app

--- app/src/main/java/com/jj/agrotech/ui/adapters/PlantaAlertsAdapter.kt
+++ app/src/main/java/com/jj/agrotech/ui/adapters/PlantaAlertsAdapter.kt
@@ -2,7 +2,9 @@ package com.jj.agrotech.ui.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.jj.agrotech.R
 import com.jj.agrotech.data.local.entity.Planta
 import com.jj.agrotech.databinding.ItemPlantAlertBinding
 import com.jj.agrotech.databinding.ItemPlantAlertLineBinding
@@ -36,23 +38,31 @@ class PlantaAlertsAdapter (
         holder.binding.llAlertContainer.removeAllViews()
 
         alerts.forEach { alert ->
+            val context = holder.binding.root.context
+
             val lineBinding = ItemPlantAlertLineBinding.inflate(
-                LayoutInflater.from(holder.binding.root.context),
+                LayoutInflater.from(context),
                 holder.binding.llAlertContainer,
                 false
             )
 
-            lineBinding.tvAlertType.text = alert.severity.name
+            val (severityColor, textTypeColor) = when (alert.severity) {
+                AlertSeverity.ALERT -> ContextCompat.getColor(context,R.color.alert) to ContextCompat.getColor(context, R.color.alert_text)
+                AlertSeverity.WARNING -> ContextCompat.getColor(context,R.color.warning) to ContextCompat.getColor(context, R.color.warning_text)
+                AlertSeverity.RECOMENDED -> ContextCompat.getColor(context,R.color.recomended) to ContextCompat.getColor(context, R.color.recomended_text)
+            }
+
+            lineBinding.tvAlertType.text = when (alert.severity) {
+                AlertSeverity.ALERT -> "Perigo"
+                AlertSeverity.WARNING -> "Alerta"
+                AlertSeverity.RECOMENDED -> "Recomendado"
+            }
+            lineBinding.tvAlertType.setTextColor(textTypeColor)
+
 
             lineBinding.tvAlertMessage.text = alert.message
 
-            val color = when (alert.severity) {
-                AlertSeverity.ALERT -> android.graphics.Color.RED
-                AlertSeverity.WARNING -> android.graphics.Color.YELLOW
-                AlertSeverity.RECOMENDED -> android.graphics.Color.GREEN
-            }
-
-            lineBinding.viewSeverityIndicator.setBackgroundColor(color)
+            lineBinding.viewSeverityIndicator.setBackgroundColor(severityColor)
 
             holder.binding.llAlertContainer.addView(lineBinding.root)
         }
