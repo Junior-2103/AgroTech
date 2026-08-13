a2f2657ae54a18585f0c7e0a1a6603054ccfe798
Junior-2103 <jjosewilker45@proton.me>
2026-08-13 17:41:27 -0300

    refactor: Renomear variáveis de cores, para melhor visualização no código

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
 
-const black = Color(0xFF000000);
-const white = Color(0xFFFFFFFF);
+const blackColor = Color(0xFF000000);
+const whiteColor = Color(0xFFFFFFFF);
 
-const background = Color(0xFFEBDACA);
-const text = Color(0xFF3A5642);
+const backgroundColor = Color(0xFFEBDACA);
+const textColor = Color(0xFF3A5642);
 
-const accent = Color(0xFF2F7A4F);
-const surface = Color(0xFFFEFFEF);
+const accentColor = Color(0xFF2F7A4F);
+const surfaceColor = Color(0xFFFEFFEF);
 
-const secondary = Color(0xFFD68B3E);
-const onSecondary = Color(0xFF1A1A1A);
+const secondaryColor = Color(0xFFD68B3E);
+const onSecondaryColor = Color(0xFF1A1A1A);
 
-const alert = Color(0xFFB3413A);
-const alertText = Color(0xFF9A362F);
+const alertColor = Color(0xFFB3413A);
+const alertTextColor = Color(0xFF9A362F);
 
-const warning = Color(0xFFCA833A);
-const warningText = Color(0xFF96612A);
+const warningColor = Color(0xFFCA833A);
+const warningTextColor = Color(0xFF96612A);
 
-const recomended = Color(0xFF2F7A4F);
-const recomendedText = Color(0xFF266341);
+const recomendedColor = Color(0xFF2F7A4F);
+const recomendedTextColor = Color(0xFF266341);
