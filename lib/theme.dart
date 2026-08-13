9d44ae37fc5fce29e37aa90019efcf2054efd46d
Junior-2103 <jjosewilker45@proton.me>
2026-08-12 21:49:13 -0300

    chore: Migrar arquivos e configurações para o projeto Flutter

---
+++ lib/theme.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+const black = Color(0xFF000000);
+const white = Color(0xFFFFFFFF);
+
+const background = Color(0xFFEBDACA);
+const text = Color(0xFF3A5642);
+
+const accent = Color(0xFF2F7A4F);
+const surface = Color(0xFFFEFFEF);
+
+const secondary = Color(0xFFD68B3E);
+const onSecondary = Color(0xFF1A1A1A);
+
+const alert = Color(0xFFB3413A);
+const alertText = Color(0xFF9A362F);
+
+const warning = Color(0xFFCA833A);
+const warningText = Color(0xFF96612A);
+
+const recomended = Color(0xFF2F7A4F);
+const recomendedText = Color(0xFF266341);
