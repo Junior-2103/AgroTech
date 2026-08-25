5bfa7860e1e33d2870c8620d578e84f7b18d4233
Junior-2103 <jjosewilker45@proton.me>
2026-08-25 12:24:45 -0300

    fix: Recorrigir o flake.nix

--- flake.nix
+++ flake.nix
@@ -14,8 +14,16 @@
         packages = [
           pkgs.flutter
           pkgs.jdk17
+          pkgs.android-tools
         ];
+        shellHook = ''
+          export JAVA_HOME="${pkgs.jdk17}"
+          export ANDROID_HOME="$HOME/Android/Sdk"
+          export ANDROID_SDK_ROOT="$ANDROID_HOME"
+          export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/cmdline-tools/latest/bin:$PATH"
+        '';
       };
+      
     };
   };
 }
